e6206b964e8c14386bdf315f46cf04176a7e77bc
Kargin Maxim <88215412+parasite-karginmaxx@users.noreply.github.com>
2023-02-27 19:18:33 +0000

    Copy Pair.cs

--- ClassLibrary1/Pair.cs
+++ ClassLibrary1/Pair.cs
@@ -1,6 +1,17 @@
-﻿namespace ClassLibrary1
+﻿namespace ClassLibrary1;
+
+public class Pair
 {
-    public class Pair
+    private DateTime start;
+    private DateTime end;
+    private DateTime breakStart;
+    private DateTime breakEnd;
+    private Shift shift;
+    public Pair(DateTime start, DateTime end, DateTime breakStart, DateTime breakEnd)
     {
-    }
+        this.start = start;
+        this.end = end;
+        this.breakStart = breakStart;
+        this.breakEnd = breakEnd;
+        shift = new Shift();
 }
