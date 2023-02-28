d558355cc8246a75a490ebdffa6a2ab0780e5fbe
Danya <123678517+Snorksk@users.noreply.github.com>
2023-02-28 12:41:38 +0300

    Update Journal.cs

--- ClassLibrary1/Journal.cs
+++ ClassLibrary1/Journal.cs
@@ -5,17 +5,18 @@ namespace ClassLibrary1;
 public class Journal
 {
 	private int? mark;
-	private int? date;
+	private readonly DateTime date;
+        public DateTime? Date { get => date; }
 	private readonly Employee teacher;
 	public Employee teacher { get => teacher; }
 	private readonly Student student;
 	public Student student { get => student; }
 	private readonly Discipline discipline;
 	public Discipline discipline { get => discipline; }
-	public Journal(int? mark, int? date, Employee teacher, Student student, Discipline discipline)
+	public Journal(int? mark, DateTime date, Employee teacher, Student student, Discipline discipline)
 	{
 		this.mark = mark;
-		this.date = date;
+		this.date = date ?? DateTime.Today;
 		this.teacher = teacher;
 		this.student = student;
 		this.discipline = discipline;
