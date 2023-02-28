dafd6ccc1e6ceeba4b8a113551920df543d9b44e
Jambo <egor.zaytsev.2018@list.ru>
2023-02-28 13:03:15 +0300

    Modify Corpus.cs

---
+++ ClassLibrary1/Corpus.cs
@@ -0,0 +1,18 @@
+﻿namespace ClassLibrary1;
+
+class Corpus
+{
+	public readonly string? title;
+	public readonly string? address;
+	private readonly Employee commandant;
+	private readonly Organization organization;
+	public Employee Commandant { get => commandant; }
+	public Organization Organization { get => organization; }
+	public Corpus(string? title, string? address, Employee commandant, Organization organization)
+	{
+		this.title = title;
+		this.address = address;
+		this.organization = organization;
+		this.commandant = commandant;
+	}
+}
