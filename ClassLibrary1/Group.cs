9395b2ed1814f19f9e55038f622942f515097b67
user <kirillnowoselow4@gmail.com>
2023-02-28 10:09:48 +0300

    Var05 Student class

---
+++ ClassLibrary1/Group.cs
@@ -0,0 +1,9 @@
+﻿namespace ClassesLibrary1
+/* https://docs.google.com/document/d/1qZEaADy71rJCOuNSTpyhh-f-NhSL2a6DCm88EgFO20o/edit */
+{
+    public class Group
+    {
+
+     
+    }
+}
---
+++ ClassLibrary1/Student.cs
@@ -0,0 +1,21 @@
+﻿//https://docs.google.com/document/d/1FrtPd2tz85cDVhZCYXqL0wrByPjNbd6kUbjXzOzfLPc/edit - задание
+namespace ClassesLibrary1
+{
+    public class Student
+    {
+        private string? name; // имя
+        private string? s_name; // фамилия
+        private string? p_name; // отчество
+        private readonly Group group;
+        public Group Group { get => group; }
+        private DateTime date; // дата рождения
+        Student(string name, string s_name, string p_name, Group group, DateTime? date) // конструктор со всей фигней
+        {
+            this.name = name;
+            this.s_name = s_name;
+            this.p_name = p_name;
+            this.group = group;
+            this.date = date ?? DateTime.Today;
+        }
+    }
+}
