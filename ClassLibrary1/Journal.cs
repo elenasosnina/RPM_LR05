cb0693b83da620eea6c4bc9804adef8000a03a9b
Danya <123678517+Snorksk@users.noreply.github.com>
2023-02-28 12:34:51 +0300

    Add files via upload

---
+++ ClassLibrary1/Journal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ClassLibrary;
+
+public class Journal
+{
+	private int? mark;
+	private int? date;
+	private readonly Employee teacher;
+	public Employee teacher { get => teacher; }
+	private readonly Student student;
+	public Student student { get => student; }
+	private readonly Discipline discipline;
+	public Discipline discipline { get => discipline; }
+	public Journal(int? mark, int? date, Employee teacher, Student student, Discipline discipline)
+	{
+		this.mark = mark;
+		this.date = date;
+		this.teacher = teacher;
+		this.student = student;
+		this.discipline = discipline;
+	}
+}
