d00444ebd13e3fb4991b5f76fb5427fdb653c365
Victorzxc <123728618+Victorzxc@users.noreply.github.com>
2023-03-05 22:24:03 +0300

    Update and rename ClassLibrary1/Loli.cs to Библиотека классов1/Loli.cs

--- Библиотека классов1/Loli.cs
+++ Библиотека классов1/Loli.cs
@@ -5,7 +5,7 @@ while (true)
     Console.WriteLine("Введите имя класса");
     switch (Console.ReadLine())
     {
-        case "Loli":
+        case "Employee":
             Console.WriteLine(CreateLoli().SName);
             Console.WriteLine(CreateLoli().Name);
             Console.WriteLine(CreateLoli().PName);
@@ -16,7 +16,7 @@ while (true)
     }
 }
 
-static Loli CreateLoli()
+static Employee CreateEmployee()
 {
     Console.Write("Введите фамилию: ");
     string? s_Name = Console.ReadLine();
@@ -24,13 +24,13 @@ static Loli CreateLoli()
     string? Name = Console.ReadLine();
     Console.Write("Введите отчество: "); 
     string? p_Name = Console.ReadLine();
-    return new Loli((s_Name, Name, p_Name));
+    return new Employee((s_Name, Name, p_Name));
 }
 
 //Класс
 namespace ClassLibrary1
 {
-    public class Loli
+    public class Employee
     {
         private readonly string s_name;
         public string SName { get => s_name; }
@@ -40,14 +40,14 @@ namespace ClassLibrary1
 
         private readonly string p_name;
         public string PName { get => p_name; }
-        public Loli(string s_name, string name, string p_name)
+        public Employee(string s_name, string name, string p_name)
         {
             this.s_name = s_name;
             this.name = name;
             this.p_name = p_name;
         }
 
-        public Loli((string? s_Name, string? Name, string? p_Name) value)
+        public Employee((string? s_Name, string? Name, string? p_Name) value)
         {
         }
     }
